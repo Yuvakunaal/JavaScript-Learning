1f89754ab31192e66f7989aa3715c22394dbf5c9
Yuva Kunaal <bhavikunaal@gmail.com>
2024-08-16 23:24:37 +0530

    m

---
+++ 15.callback&async_awaits&promises/script.js
@@ -0,0 +1,8 @@
+// Sync in JS :-
+
+// Synchronous :-
+// Synchronous means the code runs in a particular sequence of instructions given in a program. 
+// Each instruction awaits for the previous instruction to complete its execution.
+
+// Asynchronous :-
+// Due to synchronous programming, sometimes imp intstructions get blocked due to some previous instructions, // which causes a delay in the UI. Asynchronous code executes allows to execute next instructions immediately and doesn't block the flow.
